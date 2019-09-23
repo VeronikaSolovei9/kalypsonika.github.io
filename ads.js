de0dbe03b23d1bc2a5f0cce0a24acd8d01b1d83f
Veronika Solovei <kalypsonika@gmail.com>
2019-09-23 16:18:36 -0700

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,3 @@
+var videoContent = document.getElementById('contentElement');
+var playButton = document.getElementById('playButton');
+playButton.addEventListener('click', function(){ videoContent.play() });
